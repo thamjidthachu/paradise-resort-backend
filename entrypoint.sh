e76966b4d6b4b354845a9a4670bbe04e0441bf02
Thamjid M <thamjid@aqaryint.com>
2025-08-20 16:07:41 +0400

    :construction_worker: fixed errors while deploying

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,11 +3,43 @@
 # Exit on error
 set -e
 
+echo "Waiting for PostgreSQL..."
+
+# Function to test PostgreSQL connection
+postgres_ready() {
+  python << END
+import sys
+import psycopg2
+import os
+from decouple import config
+
+try:
+    psycopg2.connect(
+        dbname=config('DB_NAME'),
+        user=config('DB_USER'),
+        password=config('DB_PASSWORD'),
+        host=config('DB_HOST'),
+        port=config('DB_PORT', default=5432)
+    )
+except psycopg2.OperationalError:
+    sys.exit(1)
+sys.exit(0)
+END
+}
+
+# Wait for PostgreSQL to be ready
+until postgres_ready; do
+  echo "PostgreSQL is unavailable - sleeping"
+  sleep 1
+done
+
+echo "PostgreSQL is up - executing command"
+
 # Run migrations
 python manage.py migrate --noinput
 
-# Collect static files (optional, if you use static)
-# python manage.py collectstatic --noinput
+# Collect static files
+python manage.py collectstatic --noinput
 
 # Start server
 python manage.py runserver 0.0.0.0:8000
